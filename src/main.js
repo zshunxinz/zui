af43b5a1bc2114cb0bc4eb6ccce0f9c7dab4040c
zshunxinz <zhoushunxin01@qq.com>
2025-07-19 18:51:27 +0800

    fix: 恢复被注释的style.css导入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './styles/theme.css'
 import '@/styles/variables.css'
-// import '@/style.css'
+import '@/style.css'
 import App from '@/App.vue'
 
 let app = createApp(App)
