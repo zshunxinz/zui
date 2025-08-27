73738b69c5fbad73ec383500c88823de638715b9
zhoushunxin <zhoushunxin01@qq.com>
2025-08-27 17:29:00 +0800

    style(Select): 调整下拉箭头图标的样式和属性

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -105,19 +105,34 @@
       <!-- <div class="x-select__suffix"> -->
       <!-- <i class="x-select__arrow" :class="{ 'is-reverse': visible }"> -->
       <!-- <div> -->
-      <svg
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
         height="16"
         viewBox="0 0 24 24"
         fill="none"
         :stroke="`var(--color-${type})`"
-        stroke-width="0.8"
+        stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
         class="lucide lucide-chevron-down-icon lucide-chevron-down"
       >
         <path d="m6 9 6 6 6-6" />
+      </svg> -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="14"
+        height="14"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-chevron-down-icon lucide-chevron-down w-4 h-4 opacity-50 shrink-0"
+        aria-hidden="true"
+      >
+        <path d="m6 9 6 6 6-6"></path>
       </svg>
       <!-- </div> -->
 
